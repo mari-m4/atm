d0e6bc71a3797a59c8e2a61ca570c1f287850c23
Koichi Murakami <koichi.murakami@mac.com>
2018-09-15 13:51:54 +0900

    first commit from XCode project

---
+++ ATM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ATM
+//
+//  Created by Koichi Murakami on 9/15/18.
+//  Copyright © 2018 m4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
